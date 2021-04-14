4b74f4ed06583d88f537a6c026324e373ca9ba03
Luis Juarez <luisisthejuarez@gmail.com>
2021-04-14 01:36:40 -0400

    working

--- app/src/main/java/edu/grcc/luisjuarez/handheldsecurityterms/DictionaryActivity.java
+++ app/src/main/java/edu/grcc/luisjuarez/handheldsecurityterms/DictionaryActivity.java
@@ -7,13 +7,60 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class DictionaryActivity extends AppCompatActivity {
+    //global variables
+    List<String> terms;        //store strings make available to listView listener
+    List<String> definitions;  //store strings same as above
+    List<String> termsSorted;  //store strings (not necessary but everything else is here)
+    ListView listViewTerms;         //make this global so it is available to search menuItem
+    ArrayAdapter arrayAdapter; //same for arrayAdapter
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        //get SQLite database data
+        terms = (ArrayList<String>) getIntent().getSerializableExtra("terms");
+        definitions = (ArrayList<String>) getIntent().getSerializableExtra("definitions");
+        termsSorted = new ArrayList<String>(terms);  //resize termsSorted List
+        Collections.copy(terms,termsSorted);         //copy terms into termsSorted
+        Collections.sort(termsSorted);               //sort alphabetically termsSorted
+        //put data into listView
+        listViewTerms = (ListView) findViewById(R.id.listViewTerms);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, termsSorted);
+        listViewTerms.setAdapter(arrayAdapter);
+
+        //create a listViewTerms listener that will make toast with term and definition
+        listViewTerms.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String term = listViewTerms.getItemAtPosition(position).toString();
+                String definition = "";
+                for(int x = 0; x < terms.size(); x++){
+                    if(terms.get(x).equals(term)){
+                        definition = definitions.get(x);
+                    }
+                }
+                Toast.makeText(getApplicationContext(),
+                        term + " = " + definition,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 
     // Next two overrides will show and operate the menu
@@ -21,6 +68,26 @@ public class DictionaryActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_dictionary, menu);
+
+        //react to search icon listener
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        final SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                arrayAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
         return super.onCreateOptionsMenu(menu);
     }
 
--- app/src/main/java/edu/grcc/luisjuarez/handheldsecurityterms/QuizActivity.java
+++ app/src/main/java/edu/grcc/luisjuarez/handheldsecurityterms/QuizActivity.java
@@ -7,13 +7,85 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
+    //global variables
+    List<String> terms;  //store strings
+    List<String> definitions;  //store strings
+    String term;
+    String definition;
+    List<String> possibleDefinitions;
+    boolean cheat = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        refreshQuiz();
+
+        //set a listener to the textview that says {New Word}
+        TextView mNewWordTextview = (TextView) findViewById(R.id.textViewNewWord);
+        mNewWordTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                refreshQuiz();
+                String toastString = "Created a new word.";
+                Toast.makeText(QuizActivity.this, toastString, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //this code will react to radiobutton pushes
+        RadioGroup mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                //only run this if a radio button is clicked
+                if (null != rb && isRadioChecked()) {
+                    String toastString;
+                    //if correct
+                    if (rb.getText().toString().equals(definition)) {
+                        toastString = "Correct! " + term + " = " + definition + ". Creating a new word.";
+                        //this creates a pause and then puts a new question on the screen
+                        //     you have to run in seperate thread or it won't work right.
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Thread.sleep(3000);
+                                }   //pause for 3 seconds
+                                catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        refreshQuiz();
+                                    }
+                                });
+                            }
+                        }).start();
+                    } else {
+                        toastString = "Wrong, try again.";
+                    }
+                    Toast.makeText(QuizActivity.this, toastString, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
     }
 
     // Next two overrides will show and operate the menu
@@ -27,7 +99,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent;
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.menuExit:
                 System.exit(1);
                 break;
@@ -37,4 +109,54 @@ public class QuizActivity extends AppCompatActivity {
         return true;
     }
 
+    //This method puts new questions/answers on screen
+    void refreshQuiz() {
+        //if terms and definitions are not filled, they are passed by intent
+        if (terms == null) {
+            terms = (ArrayList<String>) getIntent().getSerializableExtra("terms");
+            definitions = (ArrayList<String>) getIntent().getSerializableExtra("definitions");
+        }
+        //get random term and definition
+        Random rand = new Random();
+        int index = rand.nextInt(terms.size());
+        term = terms.get(index);
+        definition = definitions.get(index);
+        //clear possibleDefinitions, add definition and 3 other random definitions
+        possibleDefinitions = new ArrayList();
+        possibleDefinitions.add(definition);
+        for (int x = 0; x < 3; x++) {
+            index = rand.nextInt(terms.size());
+            possibleDefinitions.add(definitions.get(index));
+        }
+        //shuffle possibleDefinitions
+        Collections.shuffle(possibleDefinitions);
+        //create link to radio group and clear all checks
+        RadioGroup mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        mRadioGroup.clearCheck();
+        //get links to textviews and radiobuttons
+        TextView textViewTerm = (TextView) findViewById(R.id.textViewTerm);
+        RadioButton radiobutton_1 = (RadioButton) findViewById(R.id.radioButton1);
+        RadioButton radiobutton_2 = (RadioButton) findViewById(R.id.radioButton2);
+        RadioButton radiobutton_3 = (RadioButton) findViewById(R.id.radioButton3);
+        RadioButton radiobutton_4 = (RadioButton) findViewById(R.id.radioButton4);
+        //set values for textviews and radiobuttons
+        if (cheat) textViewTerm.setText("Term is:    " + term + "  (" + definition + ")");
+        else textViewTerm.setText("Term is:    " + term);
+        radiobutton_1.setText(possibleDefinitions.get(0));
+        radiobutton_2.setText(possibleDefinitions.get(1));
+        radiobutton_3.setText(possibleDefinitions.get(2));
+        radiobutton_4.setText(possibleDefinitions.get(3));
+    }
+
+    //I created this method because the normal RadioButtonGroup.getCheckedRadioButtonId() == -1
+//                      has some issues!!!
+    private boolean isRadioChecked() {
+        boolean isChecked = false;
+        final RadioButton mButton1 = (RadioButton) findViewById(R.id.radioButton1);
+        final RadioButton mButton2 = (RadioButton) findViewById(R.id.radioButton2);
+        final RadioButton mButton3 = (RadioButton) findViewById(R.id.radioButton3);
+        final RadioButton mButton4 = (RadioButton) findViewById(R.id.radioButton4);
+        return mButton1.isChecked() | mButton2.isChecked() | mButton3.isChecked() | mButton4.isChecked();
+
+    }
 }
